ab8f03c1c093c1f7232d07b9bf8e31e2c7b4acc6
omo-y <46962034+omo-y@users.noreply.github.com>
2021-06-30 16:43:55 +0900

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,19 @@
+// //
+// /**
+//  * var変数
+//  */
+// var val1 ="var変数宣言";
+// console.log(val1);
+// //上書き
+// val1="var上書き";
+// console.log(val1);
+// //再宣言
+// var val1 ="var再宣言";
+// console.log(val1);
+//let変数
+let val2 = "let変数宣言";
+console.log(val2);
+//val2上書き
+val2 = "let上書き";
+console.log(val2);
+let val2 = "let変数再宣言";
