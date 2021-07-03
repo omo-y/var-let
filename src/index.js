65e3a45992f58bdbb5e1237d37c3245a4a510bd1
omo-y <46962034+omo-y@users.noreply.github.com>
2021-07-03 09:29:53 +0900

    ver3

--- src/index.js
+++ src/index.js
@@ -1,19 +1,9 @@
-// //
-// /**
-//  * var変数
-//  */
-// var val1 ="var変数宣言";
-// console.log(val1);
-// //上書き
-// val1="var上書き";
-// console.log(val1);
-// //再宣言
-// var val1 ="var再宣言";
-// console.log(val1);
-//let変数
-let val2 = "let変数宣言";
-console.log(val2);
-//val2上書き
-val2 = "let上書き";
-console.log(val2);
-let val2 = "let変数再宣言";
+import "./styles.css";
+const onClickadd = () => {
+  const inputText = document.getElementById("add-text").value;
+  alert(inputText);
+  document.getElementById("add-text").value = "";
+};
+document
+  .getElementById("add-button")
+  .addEventListener("click", () => onClickadd());
