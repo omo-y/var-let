4f478c80504ff3665348c9474a3e03cf75502ce0
omo-y <46962034+omo-y@users.noreply.github.com>
2021-07-04 17:25:11 +0900

    ver9

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import "./styles.css";
 const onClickadd = () => {
   //テキストボックスの値を取得
   const inputText = document.getElementById("add-text").value;
-  alert(inputText);
+
   document.getElementById("add-text").value = "";
 
   //div生成
@@ -15,37 +15,33 @@ const onClickadd = () => {
 
   const completeButton = document.createElement("button");
   completeButton.innerText = "完了";
-  completeButton.addEventListener("click", () => {});
-  // completeButton.addEventListener("click", () => {
-  //   alert("完了");
-  // });
-
   const deleteButton = document.createElement("button");
   deleteButton.innerText = "削除";
   deleteButton.addEventListener("click", () => {
-    const deletetarget = deleteButton.parentNode;
-    document.getElementById("incomplete-list").removeChild(deletetarget);
+    deletElement(deleteButton.parentNode);
   });
-
   completeButton.addEventListener("click", () => {
-    const completetarget = completeButton.parentNode;
-    const saveText = document.getElementById("incomplete-list");
-    console.log(saveText);
-    document.getElementById("incomplete-list").removeChild(completetarget);
-    console.log("完了1");
-    //div生成
-    const div = document.createElement("div");
-    div.className = "list-row";
-    console.log(div);
+    deletElement(completeButton.parentNode);
+
+    //完了したtodoへの追加処理
+    const addTarget = completeButton.parentNode;
+    //todo内容を取得
+    const text = addTarget.firstElementChild.innerText;
+
+    //div以下を削除
+    addTarget.textContent = null;
     //liタグ生成
     const li = document.createElement("li");
-    li.innerText = saveText;
-
-    const modosuButton = document.createElement("button");
-    modosuButton.innerText = "戻す";
-    div.appendChild(li);
-    div.appendChild(modosuButton);
-    document.getElementById("complete-list").appendChild(div);
+    li.innerText = text;
+    console.log(li);
+    const backbutton = document.createElement("button");
+    //divの子要素に内容を設定
+    backbutton.innerText = "戻す";
+    addTarget.appendChild = li;
+    addTarget.appendChild = backbutton;
+    console.log(addTarget);
+    //完了リストに追加
+    document.getElementById("complete-list").appendChild(addTarget);
   });
 
   div.appendChild(li);
@@ -53,6 +49,10 @@ const onClickadd = () => {
   div.appendChild(deleteButton);
 
   document.getElementById("incomplete-list").appendChild(div);
+};
+//要素を削除
+const deletElement = (target) => {
+  document.getElementById("incomplete-list").removeChild(target);
 };
 
 document
