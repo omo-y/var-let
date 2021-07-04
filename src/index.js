a07bf7e7f2194e7ea1d42cb328ffa39b3ff4f604
omo-y <46962034+omo-y@users.noreply.github.com>
2021-07-04 12:41:20 +0900

    ver8

--- src/index.js
+++ src/index.js
@@ -15,15 +15,37 @@ const onClickadd = () => {
 
   const completeButton = document.createElement("button");
   completeButton.innerText = "完了";
-  completeButton.addEventListener("click", () => {
-    alert("完了");
-  });
+  completeButton.addEventListener("click", () => {});
+  // completeButton.addEventListener("click", () => {
+  //   alert("完了");
+  // });
 
   const deleteButton = document.createElement("button");
   deleteButton.innerText = "削除";
   deleteButton.addEventListener("click", () => {
     const deletetarget = deleteButton.parentNode;
     document.getElementById("incomplete-list").removeChild(deletetarget);
+  });
+
+  completeButton.addEventListener("click", () => {
+    const completetarget = completeButton.parentNode;
+    const saveText = document.getElementById("incomplete-list");
+    console.log(saveText);
+    document.getElementById("incomplete-list").removeChild(completetarget);
+    console.log("完了1");
+    //div生成
+    const div = document.createElement("div");
+    div.className = "list-row";
+    console.log(div);
+    //liタグ生成
+    const li = document.createElement("li");
+    li.innerText = saveText;
+
+    const modosuButton = document.createElement("button");
+    modosuButton.innerText = "戻す";
+    div.appendChild(li);
+    div.appendChild(modosuButton);
+    document.getElementById("complete-list").appendChild(div);
   });
 
   div.appendChild(li);
